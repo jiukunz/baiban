f0c701795d45a21b64f58931e1862c5ab2ad5253
jiukunz <jiukunz@gmail.com>
2013-12-25 17:28:02 +0800

    initialize baiban with nodejs backend

---
+++ App.js
@@ -0,0 +1,42 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+// io.sockets.on('commit', function (socket) {
+//   console.log(socket);
+//   socket.emit('update', { message: socket.data});
+// });
+
+
+// io.sockets.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
+
+io.sockets.on('connection', function (socket) {
+
+  socket.on('commit', function (message){
+    console.log(message.data);
+    socket.broadcast.emit('update', message.data);  
+  });
+  
+});
+
+
